03da1a2fed14fe71007eedc683b78bc6e8fb9a91
Jovan97m <62722395+Jovan97m@users.noreply.github.com>
2020-11-24 21:16:06 +0100

    update 2

--- Glavna.js
+++ Glavna.js
@@ -1,20 +1,36 @@
 export class Glavna{
     constructor(){
         this.conteiner=null;
+        this.footer=null;
     }
     crtaj(host){
-        this.conteiner=document.createElement("div");
-        this.conteiner.className="Glavni-div";
-        host.appendChild(this.conteiner);
-
         const panel=document.createElement("div");
         panel.className="Panel";
         panel.innerHTML="PANEL"
-        this.conteiner.appendChild(panel);
+        host.appendChild(panel);
+
+        this.conteiner=document.createElement("div");
+        this.conteiner.className="Glavni-div";
+        host.appendChild(this.conteiner);
 
         const navigacija=document.createElement("div");
         navigacija.className="navigacija";
         navigacija.innerHTML="Navijacija";
         this.conteiner.appendChild(navigacija);
+
+        const glavniDeo=document.createElement("div");
+        glavniDeo.className="GlavniDeo";
+        glavniDeo.innerHTML="GlavniDeo";
+        this.conteiner.appendChild(glavniDeo);
+
+        const korpa=document.createElement("div");
+        korpa.className="Korpa";
+        korpa.innerHTML="Korpa";
+        this.conteiner.appendChild(korpa);
+
+        this.footer=document.createElement("div");
+        this.footer.className="Footer";
+        this.footer.innerHTML="Footer";
+        host.appendChild(this.footer);
     }
 }
